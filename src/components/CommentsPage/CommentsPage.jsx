eff89b1d585d066f213b5d30efbc2f3dd9a79408
jaeminhan <jaemin.h15@gmail.com>
2018-10-16 17:08:18 -0400

    webpack issue resolved - next on to theme issue

---
+++ src/components/CommentsPage/CommentsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentsPage = props => (
+ <div>
+     <button onClick={() => props.changeComponent(0)}>Click to return!</button>
+ </div>   
+);
+
+export default CommentsPage;
